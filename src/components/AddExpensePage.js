9c14b9e0d41de8d7b14ed5b39d1ad02ddbb6c73c
Reza Majidi <rezamajidizavie@gmail.com>
2018-08-02 11:46:57 +0430

    Initial commit

---
+++ src/components/AddExpensePage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+const AddExpensePage = () => (
+    <div>
+        this is create
+    </div>
+);
+export default AddExpensePage;
---
+++ src/components/ExpenseDashboard.js
@@ -0,0 +1,7 @@
+import React from 'react'
+const ExpenseDashboardPage = () => (
+    <div>
+        this is expense
+    </div>
+);
+export default ExpenseDashboardPage;
